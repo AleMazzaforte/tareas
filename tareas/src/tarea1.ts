8391a35262e57a0178a1a7fd978d5bac0fc50337
AleMazzaforte <109003400+AleMazzaforte@users.noreply.github.com>
2025-03-01 10:56:01 -0300

    Commit 2
    Tarea 1

--- tareas/src/tarea1.ts
+++ tareas/src/tarea1.ts
@@ -1,5 +1,40 @@
 
 
 
+
+let s: string = '{[({})]}';
+function isBalanced(s: string): boolean {
+  if (s.length % 2 === 0) {
+    const partners = new Map<string, string>([
+      ["{", "}"],
+      ["[", "]"],
+      ["(", ")"]
+    ]);
+
+    const caracteres: string[] = s.split("");
+    const primerMitad: string[] = [];
+    const segundaMitad: string[] = [];
+
+    for (let i = 0; i < caracteres.length / 2; i++) {
+      primerMitad.push(caracteres[i]);
+    };
+
+    for (let i = caracteres.length / 2; i < caracteres.length; i++) {
+      segundaMitad.push(caracteres[i]);
+    };
+
+    const primerMitadTransformada: string[] = primerMitad.map(caracter => partners.get(caracter) ?? "").reverse();
+
+    if (primerMitadTransformada.join('') === segundaMitad.join('')) {
+      console.log('exito');
+      return true;
+    } else {
+      console.log('error');
+      return false;
+    }
+  }
+  return false;
+}
+console.log('isbalanced', isBalanced);
   
   
